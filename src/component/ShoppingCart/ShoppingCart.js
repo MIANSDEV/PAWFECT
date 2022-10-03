509d263735a5421ed38b4d566a936252e15a613d
shahnawroz <sh.nawrose@gmail.com>
2022-10-03 16:59:37 +0600

    Shopping Cart Design

--- src/component/ShoppingCart/ShoppingCart.js
+++ src/component/ShoppingCart/ShoppingCart.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import ShoppingCartHeader from '../ShoppingCartHeader/ShoppingCartHeader.js'
+import ShoppingCartList from '../ShoppingCartList/ShoppingCartList.js'
 import './ShoppingCart.css'
 
 const ShoppingCart = () => {
   return (
     <div className='shoppingcart'>
         <ShoppingCartHeader/> 
+        <ShoppingCartList/>
     </div>
   )
 }
--- src/component/ShoppingCartList/ShoppingCartList.js
+++ src/component/ShoppingCartList/ShoppingCartList.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from "react";
+import CatFood from "../../images/p2.jpg";
+import "./ShoppingCartList.css";
 
 const ShoppingCartList = () => {
   return (
-    <div className='main-shoppingcartlist'>
-        <h3>Cart</h3>
+    <div className="cart-item-container">
+      <div className="item-info">
+        <div className="product-img">
+          <img src={CatFood} alt="cat-food" />
+        </div>
+        <div className="cart-title">
+          <p>Pedigree Puppy Dry Dog Food Chicken and Milk</p>
+        </div>
+        <div className="add-minus-quantity">
+          <i className="fas fa-minus minus"></i>
+          <input type="text" placeholder="2"/>
+          <i className="fas fa-plus add"></i>
+        </div>
+        <div className="cart-price-tag">
+              <h3>2000 Tk</h3>
+        </div>
+        
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ShoppingCartList
+export default ShoppingCartList;

1efe37928f9bf2882d1616d2fc067b544c4c0cc7
shahnawroz <sh.nawrose@gmail.com>
2022-10-08 23:55:12 +0600

    Humburger menu added

--- src/component/HumBurgerMenu/HumBurgerMenu.js
+++ src/component/HumBurgerMenu/HumBurgerMenu.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
 import "./HumBurgerMenu.css";
 import { Link } from "react-router-dom";
@@ -6,6 +6,7 @@ import { FaUserCircle } from "react-icons/fa";
 import { HiShoppingCart } from "react-icons/hi";
 
 const HumBurgerMenu = () => {
+  const [open, setOpen] = useState(true);
   return (
     <div className="main-burger-menu-container">
       <div className="mobile-menu-logo">
@@ -17,19 +18,25 @@ const HumBurgerMenu = () => {
         </Link>
       </div>
       <div className="mobile-menu-icon">
-        <GiHamburgerMenu />
+        <GiHamburgerMenu onClick={() => setOpen(!open)} />
       </div>
-      <div className="main-menu-humburger">
-        <ui>
-          <li>
-            <Link to={"/checkout"}>
+      <div className={`main-menu-humburger ${open && "hidden"}`}>
+        <ui className="burger-menu">
+          <Link to={"/checkout"}>
+            <li>
               <HiShoppingCart className="first-icn" />
-            </Link>
-          </li>
-
-          <li>
-            <FaUserCircle className="second-icn" />
-          </li>
+              <span className="text-icon-cart">|</span>
+              <span className="text-main-icon-cart">Checkout</span>
+            </li>
+          </Link>
+          <hr />
+          <Link to={"/"}>
+            <li>
+              <FaUserCircle className="second-icn" />
+              <span className="text-icon-cart">|</span>
+              <span className="text-main-icon-cart"> Profile</span>
+            </li>
+          </Link>
         </ui>
       </div>
     </div>
